51c3e63e200c4a7039e52e00935c6c2766fde69e
Caio <ceduardorv@outlook.com>
2025-03-21 10:29:35 -0300

    inicializando projeto

---
+++ src/test/java/br/com/dbc/hotel/ApiHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
